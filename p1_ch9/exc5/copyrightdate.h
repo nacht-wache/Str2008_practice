a6d67a2011d229acc27c4c6db34ac1678c4b9583
nacht-wache <nachtwache1889@outlook.com>
2021-03-19 03:41:10 +0300

    Create copyrightdate.h

---
+++ p1_ch9/exc5/copyrightdate.h
@@ -0,0 +1,36 @@
+#ifndef COPYRIGHTDATE_H
+#define COPYRIGHTDATE_H
+
+#include <string>
+#include <iostream>
+
+class CopyrightDate
+{
+public:
+    CopyrightDate() {};
+    ~CopyrightDate() {};
+
+    CopyrightDate(int inputYear, int inputMonth, int inputDay);
+
+    void readDate(int inputYear, int inputMonth, int inputDay);
+
+    int getYear() const {return year;};
+    int getMonth() const {return month;};
+    int getDay() const {return day;};
+private:
+    int year;
+    int month;
+    int day;
+    int currentYear = 2021;
+    int currentMonth = 3;
+    int currentDay = 18;
+    const int minYear = 1400;
+    const int minMonth = 1;
+    const int maxMonth = 12;
+    const int minDay = 1;
+    const int maxDay = 31;
+};
+
+std::ostream& operator<<(std::ostream& os, const CopyrightDate& outputCopyrightDate);
+
+#endif // COPYRIGHTDATE_H
