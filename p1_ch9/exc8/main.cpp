f1fba98df82412d2bfe4bb076e759297fc7ab20b
nacht-wache <nachtwache1889@outlook.com>
2021-03-20 13:08:29 +0300

    gpg check

---
+++ p1_ch9/exc8/main.cpp
@@ -0,0 +1,29 @@
+/*
+EXERCISE 8
+
+1. Create a Patron class for the library.
+2. The class will have a user's name, library card number, and library fees (if owed).
+3. Have functions that access these methods, as well as a function to set the fee of the user.
+4. Have a helper method that returns a Boolean (bool)
+depending on whether or not the user owes a fee
+
+*/
+
+/*
+EXERCISE 9
+
+1. Create a Library class.
+2. Include vectors of Books and Patrons.
+3. Include a struct called Transaction.
+4. Have it include a Book, a Patron, and a Date from the chapter.
+5. Make a vector of Transactions.
+6. Create functions to add books to the library, add patrons to the library, anci check out books.
+7. Whenever a user checks out a book,
+have the library make sure that both the user and the book are in the library.
+8. If they aren't, report an error.
+9. Then check to make sure that the user owes no fees.
+10. If the user does, report an error.
+11. If not, create a Transaction, and place it in the vector of Transactions.
+12. Also create a method that will return a vector that
+contains the names ofall Patrons who owe fees
+*/
